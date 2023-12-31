7d890b23bbd247b35c64523ccec73c97d4d09e6f
Michael Mroz <michaelmroz01111@gmail.com>
2023-12-31 19:50:27 -0400

    *untested* advance() method complete

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -18,6 +18,10 @@ public class Board {
         }
     }
 
+    public void fixedState(int[][] givenState){
+        state = givenState;
+    }
+
     public void randomState(){
         for( int i = 0; i < state.length; i++){
             for (int k = 0; k < state[i].length;k++){
@@ -44,64 +48,75 @@ public class Board {
     }
 
     public void advance(){
-
-        /*
-        handleEdgeRow("top")
-        //for int i = 1; i < rows.length -2; i++
-            handleLeftmost(i)
-            for int k = 1; k < cols.length - 2; k++
-               check all 8 neighbours
-            handleRightMost(i)
-        * */
-        //handleEdgeRow("bottom")
-        //state = nextState;
+        int lastCol = state[0].length -1;
+        if (state.length == 1) {
+            //if only 1 row, [0] dies always. [1] -> [last-1] have 2 neighbours, last dies
+            nextState[0][0] = 0;
+            nextState[0][lastCol] = 0;
+            for (int i = 1; i < lastCol; i++) {
+                int[] neighbours = {state[0][i - 1], state[0][i + 1]};
+                nextState[0][i] = nextStatus(state[0][i], neighbours);
+            }
+            return;
+        }
+        if (lastCol == 0){
+            // TODO handle game w/ column size 1
+            //if 1 column, top/bot row die, rest stay as they are, then all die (i think?)
+            return;
+        }
+        handleEdgeRow("top");
+        for (int i = 1; i < state.length -2; i++){
+            handleLeftMost(i);
+            for (int k = 1; k < lastCol - 1; k++) {
+                //state[i][k] = current cell
+                //each cell has neighbours: above-left, above, above-right, right, below-right, below, below-left, left
+                int[] neighbours = {
+                        state[i-1][k-1], state[i-1][k], state[i-1][k+1], state[i][k+1],
+                        state[i+1][k+1],state[i+1][k], state[i+1][k-1], state[i][k-1]
+                };
+                nextState[i][k] = nextStatus(state[i][k], neighbours);
+            }
+            handleRightMost(i);
+          }
+        handleEdgeRow("bottom");
+        state = nextState;
     }
 
 
-    public void handleLeftMost(int rowNum){
+    public void handleLeftMost(int rowIndex){
+        int i = rowIndex;
         //cell has neighbours: above, above-right, right, below-right, below
+        int[] neighbours = {state[i -1][0], state[i-1][1], state[i][1], state[i+1][1], state[i+1][0]};
+        nextState[i][0] = nextStatus(state[i][0], neighbours);
     }
-    public void handleRightMost(int rowNum){
+    public void handleRightMost(int rowIndex){
+        int i = rowIndex;
+        int k = state[i].length -1;
         //cell has neighbours: above, above-left, left, below-left, below
+        int[] neighbours = {state[i-1][k], state[i-1][k-1], state[i][k - 1], state[i+1][k -1], state[i+1][k]};
+        nextState[i][k] = nextStatus(state[i][k], neighbours);
     }
 
     public void handleEdgeRow(String topOrBottom){
         int currentRow = topOrBottom.equals("top") ? 0 : state.length - 1; //index of current row
         int verticalRow = topOrBottom.equals("top") ? 1 : state.length - 2; //index of vertical neighbour
         int last = state[0].length - 1;
-        // if last == 0 {
-        //  handleOneColumn();
-        //  return;
-        //if 1 column, top/bot row die, rest stay as they are, then all die (i think?) handle that edge case
-        // }
 
 
         //[0] has neighbours: right, below-right, below
         //last has neighbours: left, below-left, below
         //rest have neighbours: left, below-left, below, below-right, right
-        if (state.length == 1) {
-            //if only 1 row, [0] dies always. [1] -> [last-1] have 2 neighbours, last dies
-            nextState[0][0] = 0;
-            nextState[0][last] = 0;
-            for (int i = 1; i < last; i++){
-                int[] neighbours = {state[0][i-1], state[0][i+1]};
-                nextState[0][i] = nextStatus( state[0][i], neighbours);
-            }
-
-        } else { //more than 1 row
-            int[] leftMostsNeighbours = {state[currentRow][1], state[verticalRow][1], state[verticalRow][0]};
-            nextState[currentRow][0] = nextStatus(state[currentRow][0], leftMostsNeighbours);
+            int[] neighboursOfFarLeft = {state[currentRow][1], state[verticalRow][1], state[verticalRow][0]};
+            nextState[currentRow][0] = nextStatus(state[currentRow][0], neighboursOfFarLeft);
             for (int i = 1; i < last; i++){
                 //left, below-left, below, below-right, right
                 int[] neighbours = {state[currentRow][i-1], state[verticalRow][i-1], state[verticalRow][i], state[verticalRow][i+1], state[currentRow][i+1]};
                 nextState[currentRow][i] = nextStatus(state[currentRow][i], neighbours);
             }
-            int[] rightMostNeighbours = {state[currentRow][last -1],state[verticalRow][last -1], state[verticalRow][last]};
-            nextState[currentRow][last] = nextStatus(state[currentRow][last], rightMostNeighbours);
+            int[] neighboursOfFarRight = {state[currentRow][last -1],state[verticalRow][last -1], state[verticalRow][last]};
+            nextState[currentRow][last] = nextStatus(state[currentRow][last], neighboursOfFarRight);
         }
 
-    }
-    
     public int nextStatus(int currentStatus, int[] neighbours){
         int living = 0;
         int nextStatus = 0;
